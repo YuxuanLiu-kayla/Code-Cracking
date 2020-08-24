fa37991a01b565b96eb8d0670511b2ce25fa8099
YuxuanLiu-kayla <yuxuan410liu@gmail.com>
2020-08-24 22:41:18 +0800

    Update: No.3 - Unique Binary Search Trees(incomplete)

---
+++ src/myAnswers/UniqueBinarySearchTrees.java
@@ -0,0 +1,16 @@
+package myAnswers;
+
+public class UniqueBinarySearchTrees {
+	
+	public static int numTrees(int n) {
+		
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		double test = Math.log10(3) / Math.log10(2);
+		System.out.println((int) test);
+
+	}
+
+}
