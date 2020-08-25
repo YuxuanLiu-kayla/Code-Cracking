2101ff308464b901d624283208a8ea02293c673a
YuxuanLiu-kayla <yuxuan410liu@gmail.com>
2020-08-25 22:13:11 +0800

    Update: No.3 - Unique Binary Search Trees(Java)

--- src/myAnswers/UniqueBinarySearchTrees.java
+++ src/myAnswers/UniqueBinarySearchTrees.java
@@ -2,15 +2,36 @@ package myAnswers;
 
 public class UniqueBinarySearchTrees {
 	
-	public static int numTrees(int n) {
+	public int numTrees(int n) {
 		
-		return 1;
+		// Base case
+		int num = 0;
+		if (n == 1) {
+			num++;
+			return num;
+		}
+		if (n == 2) {
+			num += 2;
+			return num;
+		}
+		
+		// Recursion
+		else {  // 3 <= n <= 19
+			num += numTrees(n - 1) * 2;
+			if (n % 2 == 0) {  // n is an even integer
+				for (int i = 2; i <= n / 2; i++) {
+					num += numTrees(n - i) * numTrees(i - 1) * 2;
+				}
+			}
+			else {  // n is an odd integer
+				for (int j = 2; j <= n / 2; j++) {  // 3 / 2 = 1
+					num += numTrees(n - j) * numTrees(j - 1) * 2;
+				}
+				num += Math.pow(numTrees(n / 2), 2);
+			}
+			return num;
+		}
 	}
 
-	public static void main(String[] args) {
-		double test = Math.log10(3) / Math.log10(2);
-		System.out.println((int) test);
-
-	}
 
 }
